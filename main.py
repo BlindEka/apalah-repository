0f8d80d456fe57b5b2a1c616abab82cc92d4364d
ekazero99 <ekazero99@gmail.com>
2025-01-13 16:33:23 +0700

    methodcalculate telah diberi algoritma untuk menyelasaikan kalkulasi berdasakan operator dan sudah mendukung sekitar 8 operator

--- main.py
+++ main.py
@@ -2,7 +2,43 @@ class calculator:
   def __init__(self,number:str) ->None:
     self.number = number
   def calculate(self) ->int:
-    #calculating...
+    tokenizedNumber = self.number.split(' ')
+    result = 0
+    operator = None
+    first = True
+    for i in tokenizedNumber:
+      if first:
+        result += int(i)
+        first = False
+      elif operator:
+        if operator == '+':
+          result += int(i)
+          operator = None
+        elif operator == '-':
+          result -= int(i)
+          operator = None
+        elif operator == '*':
+          result *= int(i)
+          operator = None
+        elif operator == '/':
+          result /= int(i)
+          operator = None
+        elif operator == '**':
+          result **= int(i)
+          operator == None
+        elif operator == '%':
+          result %= int(i)
+          operator = None
+        elif operator == '//':
+          result //= int(i)
+          operator = None
+        elif operator == '^':
+          result ^= int(i)
+          operator = None
+        else:
+          raise TypeError(f'unknown operator {operator}')
+      else:
+        operator = i
     return result
 
 def coba():
